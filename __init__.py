a259373a7f32f05f671b20103ec9845716979a50
christophernagel <christopherrrrnagel@gmail.com>
2025-06-06 10:47:02 -0700

    Major reorganization: Archive deprecated code, update data collection pipeline

--- __init__.py
+++ __init__.py
@@ -1,8 +1,14 @@
-from .deprecated_core import (
-    OnlineStats,
-    FeatureNormalizer,
-    EnhancedEncodedMeasurement,
-    BuoyNode,
-    SpatialIndex,
-    SwellTracker
+"""
+Swell Tracker - Physics-informed wave forecasting system
+"""
+from .core import (
+    BuoyNetwork,
+    WaveState,
+    WavePhysics,
+    SwellAnalyzer
 )
+from .wave_transformer import EnhancedWaveTransformer
+from .wave_sequencer import WaveSequenceDataset, create_dataloaders
+from .model_trainer import WaveModelTrainer
+
+__version__ = "0.1.0"
